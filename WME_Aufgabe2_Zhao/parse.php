a36202d3d0f230cb1ed8e36de3b81b7f6338e231
Yiming Zhao <39445366+HarryForSWT@users.noreply.github.com>
2021-11-14 00:33:26 +0100

    include -- require

--- WME_Aufgabe2_Zhao/parse.php
+++ WME_Aufgabe2_Zhao/parse.php
@@ -1,5 +1,5 @@
 <?php 
-include "world_data_parser.php"; //Datei suchen/importieren
+require "world_data_parser.php"; //Datei suchen/importieren
 $wdp1 = new WorldDataParser(); //Objekt von Klasse WorldDataParser erzeugen
 $csv = $wdp1 ->parseCSV("world_data_v1.csv"); //Funktionaufruf "Parsen" durch Objekt $wpd1
 echo '<pre>' , print_r($csv) , '</pre>'; //Ausgabe der Arrays.
